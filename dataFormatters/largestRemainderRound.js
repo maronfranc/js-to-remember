d43c33d69e8c6d3ec43c8b6aacb75340cd80faa9
Máron <maron.franca@gmail.com>
2020-04-15 10:55:35 -0300

    Sort fix and example

--- dataFormatters/largestRemainderRound.js
+++ dataFormatters/largestRemainderRound.js
@@ -1,6 +1,7 @@
-const sumHelper = (arr) => arr.reduce((acc, val) => acc + val, 0);
 const PERCENT = 100;
 const ONE = 1;
+const ZERO = 0;
+const sumHelper = (arr) => arr.reduce((acc, val) => acc + val, ZERO);
 
 const separateDecimalsFromArrayOfNumbers = (arrNumbers) => {
   let integers = [];
@@ -20,11 +21,22 @@ const largestRemainderRound = (arrNumbers) => {
   const diff = PERCENT - roundedSum;
 
   const decimalsCopy = [...decimals];
-  decimalsCopy.sort().reverse();
-  for (let i = 0; i < diff; i++) {
+  decimalsCopy.sort((a, b) => b - a);
+  for (let i = ZERO; i < diff; i++) {
     const index = decimals.indexOf(decimalsCopy[i]);
     integers[index] += ONE;
   }
 
   return integers;
 };
+
+const arr = [
+  31.655255739025375,
+  23.157470801449858,
+  20.338300443012486,
+  5.2356020942408374,
+  19.653644784534837,
+];
+const result = largestRemainderRound(arr);
+console.log("Result", result);
+console.log(`Percentage: ${sumHelper(result)}%`);
